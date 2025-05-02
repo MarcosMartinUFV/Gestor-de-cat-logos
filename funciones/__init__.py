c90e2be1e150b6ef98cd9f336de18807d156fec4
DiegoMedieoDomingo <9302308@alumnos.ufv.es>
2025-05-02 20:19:43 +0200

    modificaciones en _init_.py

---
+++ funciones/__init__.py
@@ -0,0 +1,2 @@
+#Este archivo permite que la carpeta funciones actúe como un paquete de Python
+#Este archivo puede estar vacío si no necesitas lógica al importar el módulo, pero es obligatorio para que Python reconozca funciones/ como un paquete.
