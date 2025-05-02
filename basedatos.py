f2b55458f182655898e1bc5a3d8fd8de3238e7c3
MarcosMartinUFV <9305571@alumnos.ufv.es>
2025-05-02 20:30:26 +0200

    Correccion en base de datos, por la version de SQLAlchemy 1.4.54

--- basedatos.py
+++ basedatos.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()  # Clase base para modelos ORM
 
-engine = create_engine("sqlite:///catalogo.db", echo=True)
-Session = sessionmaker(bind=engine)
+engine = create_engine("sqlite:///catalogo.db", echo=True)  # Conexión a SQLite con logging activado
+Session = sessionmaker(bind=engine)  # Creador de sesiones
