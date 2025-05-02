10bef4a265feb307fb43fa023b1dec677c1a548e
MarcosMartinUFV <9305571@alumnos.ufv.es>
2025-05-02 20:26:14 +0200

    Clase Producto limpiada tras generación automática con sqlacodegen

--- tablas/producto.py
+++ tablas/producto.py
@@ -1,11 +1,14 @@
 from sqlalchemy import Column, Integer, String
-from basedatos import Base  # Importa la clase Base desde basedatos.py
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class Producto(Base):
     __tablename__ = 'producto'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    nombre = Column(String(100), nullable=False)
-    descripcion = Column(String(255))
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String, nullable=False)
+    descripcion = Column(String)
     precio = Column(Integer, nullable=False)
     stock = Column(Integer, nullable=False)
+
